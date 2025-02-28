f14f8e164fc597007966f3cfe8582ef76ad91b9b
WyattAlexander-001 <wyattbushman117@gmail.com>
2025-02-28 02:59:02 -0500

    finished voting app
    It's simple and can be extended

--- _playgrounds/sinatra_playground/voting_app/suffragist.rb
+++ _playgrounds/sinatra_playground/voting_app/suffragist.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'yaml/store'
 
 Choices = {
   'HAM' => 'Hamburger',
@@ -7,6 +8,29 @@ Choices = {
   'NOO' => 'Noodles',
 }
 
+# Display the voting form
 get '/' do
+  @title = 'Welcome to the Suffragist!'
   erb :index
+end
+
+# Ability to cast a vote
+post '/cast' do
+  @title = 'Thanks for casting your vote!'
+  @vote  = params['vote'] # The vote is stored in the params hash
+  @store = YAML::Store.new 'votes.yaml' # Store the votes in a YAML file
+  @store.transaction do 
+    @store['votes'] ||= {} # If there are no votes, create an empty hash
+    @store['votes'][@vote] ||= 0 # If there are no votes for this choice, set it to 0
+    @store['votes'][@vote] += 1 # Increment the vote count
+  end
+  erb :cast
+end
+
+# Display the results
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yaml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
 end
